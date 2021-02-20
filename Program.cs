73484f269a1fc2f9a04fdb642c61f36d27779377
Ohi Uii <ohiyoaodesu@gmail.com>
2021-02-20 18:55:05 +0700

    Nuttawan HW2 finish

--- Program.cs
+++ Program.cs
@@ -294,9 +294,14 @@ namespace DNWS
                     // Wait for client
                     clientSocket = serverSocket.Accept();
                     // Get one, show some info
-                    _parent.Log("Client accepted:" + clientSocket.RemoteEndPoint.ToString());
+                    //_parent.Log("Client accepted:" + clientSocket.RemoteEndPoint.ToString());
                     HTTPProcessor hp = new HTTPProcessor(clientSocket, _parent);
-                    hp.Process();
+                    //hp.Process(); //move to use when thread start
+
+                    // Implement Threads
+                    Thread t = new Thread(new ThreadStart(hp.Process)); //use hp.Process when thread start
+                    t.Start();
+
                 }
                 catch (Exception ex)
                 {
